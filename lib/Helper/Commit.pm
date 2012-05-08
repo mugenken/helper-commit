acdcd5129407a0a985f527dcc5a59cdabe4ecd67
Mugen Kenichi <mugen.kenichi@uninets.eu>
2012-05-08 11:57:10 +0200

    version bump

--- lib/Helper/Commit.pm
+++ lib/Helper/Commit.pm
@@ -330,7 +330,7 @@ This is an unstable development release not ready for production!
 
 =head1 VERSION
 
-Version 0.000001
+Version 0.000002
 
 =head1 SYNOPSIS
 
--- lib/Helper/Commit/Version.pm
+++ lib/Helper/Commit/Version.pm
@@ -12,7 +12,7 @@ sub new {
 
 sub get {
     my $self    = shift;
-    my $VERSION = version->declare('0.00.01')->numify;
+    my $VERSION = version->declare('0.00.02')->numify;
     return $VERSION;
 }
 
@@ -30,7 +30,7 @@ This is an unstable development release not ready for production!
 
 =head1 VERSION
 
-Version 0.000001
+Version 0.000002
 
 =head1 SYNOPSIS
 
